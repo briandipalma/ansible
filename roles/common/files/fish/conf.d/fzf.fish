19de4e9ab178581104059645425b6e2ad3d19a75
Brian Di Palma <briandipalma [at] outlook.com>
2021-06-03 14:00:34 +0100

    feat(fish): Add support for specifying a directory when running Ctrl+T fzf command

--- roles/common/files/fish/conf.d/fzf.fish
+++ roles/common/files/fish/conf.d/fzf.fish
@@ -1,2 +1,2 @@
-set -gx FZF_DEFAULT_COMMAND 'fd --type f'
-set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -gx FZF_DEFAULT_COMMAND "fd --type f"
+set -gx FZF_CTRL_T_COMMAND "fd --type f . \$dir"
