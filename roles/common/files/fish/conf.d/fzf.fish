03c9e237fdeeb748238282a71db7ffe3d9ad7cda
Brian Di Palma <briandipalma [at] outlook.com>
2021-06-08 21:57:14 +0100

    feat(fish): Add fzf default opts - multi and keybindings

--- roles/common/files/fish/conf.d/fzf.fish
+++ roles/common/files/fish/conf.d/fzf.fish
@@ -1,3 +1,4 @@
+set -gx FZF_DEFAULT_OPTS "--multi --bind='ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-a:select-all'"
 set -gx FZF_DEFAULT_COMMAND "fd --type f"
 set -gx FZF_CTRL_T_COMMAND "fd --type f . \$dir"
 set -gx FZF_ALT_C_COMMAND "fd --type d . \$dir"
