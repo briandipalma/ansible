3c6de5c7f87bed669bdbeac794def7414ee2cfc9
Brian Di Palma <briandipalma [at] outlook.com>
2021-06-11 16:55:59 +0100

    feat(fish): Handle git worktrees and opening lazygit from any directory in a git repo

--- roles/common/files/fish/functions/open-lazygit-tmux-popup.fish
+++ roles/common/files/fish/functions/open-lazygit-tmux-popup.fish
@@ -1,3 +1,16 @@
 function open-lazygit-tmux-popup
-  tmux popup -E -w 90% -h 90% "lazygit -p $PWD"
+  set repo_root $PWD
+
+  # While not at the root of a git repo and the root of the filesystem
+  while not test -e $repo_root/.git && test (dirname $repo_root) != $repo_root
+    set repo_root (dirname $repo_root)
+  end
+
+  if test -d $repo_root/.git
+    # In a standard git repo
+    tmux popup -E -w 90% -h 90% "lazygit -p $repo_root"
+  else if test -f $repo_root/.git
+    # In a git worktree
+    tmux popup -E -w 90% -h 90% "lazygit -w $repo_root"
+  end
 end
