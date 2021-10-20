d26f6fd58cccfb4e835f5068dba6256e07cc5aa1
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-10-20 22:34:38 +0100

    refactor(nvim): Move nvim-tree setup to plugin lua file

--- roles/common/files/nvim/lua/plugins.lua
+++ roles/common/files/nvim/lua/plugins.lua
@@ -11,8 +11,7 @@ return require('packer').startup(function(use)
   -- File explorer
   use {
     'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = function() require'nvim-tree'.setup {} end
+    requires = 'kyazdani42/nvim-web-devicons'
   }
   -- Fuzzy finder over lists
   use {
--- roles/common/files/nvim/plugin/nvim-tree.lua
+++ roles/common/files/nvim/plugin/nvim-tree.lua
@@ -1,3 +1,5 @@
+require'nvim-tree'.setup {}
+
 -- Key mappings
 vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", {})
 vim.api.nvim_set_keymap("n", "<leader>n", ":NvimTreeFindFile<CR>", {})
