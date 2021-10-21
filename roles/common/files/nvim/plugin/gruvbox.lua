3344a4e94c72262c0505a0c463ffeb53f4120270
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-10-21 22:26:18 +0100

    feat(nvim): Enable italics in gruvbox

--- roles/common/files/nvim/plugin/gruvbox.lua
+++ roles/common/files/nvim/plugin/gruvbox.lua
@@ -1,3 +1,4 @@
-vim.cmd('colorscheme gruvbox')
-vim.cmd('set background=light')
 vim.api.nvim_set_var('gruvbox_contrast_light', "hard")
+vim.api.nvim_set_var('gruvbox_italic', "1")
+vim.cmd('set background=light')
+vim.cmd('colorscheme gruvbox')
