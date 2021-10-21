adbfceaa7ae96a0a55448d50cf40430ca182b6cc
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-10-21 22:27:02 +0100

    feat(nvim): Add keymap to open a terminal

--- roles/common/files/nvim/lua/keymaps.lua
+++ roles/common/files/nvim/lua/keymaps.lua
@@ -1,2 +1,4 @@
+-- Open a terminal, in a horizontal row at the bottom
+vim.api.nvim_set_keymap('n', '<leader>tt', '<C-w>s<C-w>J:ter<CR>i', {})
 -- Easier writing
 vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', {})
