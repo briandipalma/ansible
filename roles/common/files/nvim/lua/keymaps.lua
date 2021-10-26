8427c1c118bfc915817798d355946df1d226cbbf
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-10-26 16:39:49 +0100

    feat(nvim): Add a keymap to hide the selected terminal

--- roles/common/files/nvim/lua/keymaps.lua
+++ roles/common/files/nvim/lua/keymaps.lua
@@ -1,4 +1,6 @@
 -- Open a terminal, in a horizontal row at the bottom
 vim.api.nvim_set_keymap('n', '<leader>tt', '<C-w>s<C-w>J:ter<CR>i', {})
+-- Hide the current terminal
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n><C-w>c', {})
 -- Easier writing
 vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', {})
