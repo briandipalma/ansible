bbdb24ff4816461ed70449f89285d4f976341456
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-11-08 10:15:01 +0000

    refactor(nvim): Remove fixjson formatter as Prettier formats JSON

--- roles/common/files/nvim/plugin/null-ls.nvim.lua
+++ roles/common/files/nvim/plugin/null-ls.nvim.lua
@@ -3,7 +3,6 @@ local null_ls = require("null-ls")
 local sources = {
 	null_ls.builtins.formatting.eslint_d,
 	null_ls.builtins.formatting.fish_indent,
-	null_ls.builtins.formatting.fixjson,
 	null_ls.builtins.formatting.prettierd,
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.formatting.terraform_fmt,
