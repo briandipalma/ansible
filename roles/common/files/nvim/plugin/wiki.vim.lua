0041bff43119046aa7a85084537e62afadb2a325
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-11-16 20:55:41 +0000

    feat(nvim): Make wiki link type md

--- roles/common/files/nvim/plugin/wiki.vim.lua
+++ roles/common/files/nvim/plugin/wiki.vim.lua
@@ -1,3 +1,5 @@
-vim.g.wiki_root = "~/wiki"
+vim.g.wiki_root = "~/dev/my-data/wiki"
 vim.g.wiki_filetypes = { "md" }
 vim.g.wiki_link_extension = ".md"
+vim.g.wiki_link_target_type = "md"
+vim.g.wiki_write_on_nav = 1
