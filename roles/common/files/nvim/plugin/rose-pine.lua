93727833fc7b60518a67598b23918732dd36883f
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-12-03 17:51:44 +0000

    feat(nvim): Change inactive window background

--- roles/common/files/nvim/plugin/rose-pine.lua
+++ roles/common/files/nvim/plugin/rose-pine.lua
@@ -1,4 +1,6 @@
 vim.g.rose_pine_variant = "dawn"
+vim.g.rose_pine_bold_vertical_split_line = true
+vim.g.rose_pine_inactive_background = true
 
 vim.cmd("set background=light")
 vim.cmd("colorscheme rose-pine")
