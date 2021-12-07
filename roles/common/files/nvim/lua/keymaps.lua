4610ed959f2d450cdd223040dfc91c0e594203d8
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-12-07 14:07:48 +0000

    feat(nvim): Always write when asked to

--- roles/common/files/nvim/lua/keymaps.lua
+++ roles/common/files/nvim/lua/keymaps.lua
@@ -1,7 +1,7 @@
 -- Exit from terminal insert mode
 vim.api.nvim_set_keymap("t", "<C-\\>", "<C-\\><C-n>", {})
 -- Easier writing
-vim.api.nvim_set_keymap("n", "<leader>s", ":update<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>s", ":w<CR>", {})
 -- Move focused window
 vim.api.nvim_set_keymap("n", "<Left>", "<C-w>h", {})
 vim.api.nvim_set_keymap("n", "<Right>", "<C-w>l", {})
