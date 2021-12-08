1f5d591c433fadc59e935703da0d3fcbf9febd89
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-12-08 17:35:22 +0000

    feat(nvim): Display diagnostics source and add a border around floats

--- roles/common/files/nvim/lua/diagnostic.lua
+++ roles/common/files/nvim/lua/diagnostic.lua
@@ -1,9 +1,9 @@
 local M = {}
 
-function M.show_line_diagnostics()
-	local opts = { border = "rounded", source = "always" }
+local float_opts = { border = "rounded", source = "always" }
 
-	vim.diagnostic.open_float(nil, opts)
+function M.show_line_diagnostics()
+	vim.diagnostic.open_float(nil, float_opts)
 end
 
 -- Change diagnostic symbols in gutter
@@ -13,5 +13,10 @@ for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+vim.diagnostic.config({
+	virtual_text = { source = "always" },
+	float = float_opts,
+})
 
 return M
