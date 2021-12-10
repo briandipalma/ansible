b5fd70e3f43ac7176bfd36762b402411126c9b26
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-12-10 10:49:51 +0000

    feat(fish): Remove fish from shell title

--- roles/common/files/fish/functions/fish_title.fish
+++ roles/common/files/fish/functions/fish_title.fish
@@ -1,5 +1,5 @@
 function fish_title
-    set -q argv[1]; or set argv fish
-    # Looks like `~/d/fish: git log` or `/e/apt: fish`
-    echo (fish_prompt_pwd_dir_length=1 prompt_pwd): $argv
+    set -q argv[1]; and set command ": $argv"; or set command ""
+    # Looks like `~/d/fish: git log` or `/e/apt`
+    echo (fish_prompt_pwd_dir_length=1 prompt_pwd)$command
 end
