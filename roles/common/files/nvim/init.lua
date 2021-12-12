48461dd01b19a5d642c308fed79f21bdca091db1
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-12-12 08:26:46 +0000

    feat(nvim): Add localleader mapping for experimentation

--- roles/common/files/nvim/init.lua
+++ roles/common/files/nvim/init.lua
@@ -1,5 +1,7 @@
 -- Map the leader key to space, more convenient than default `\`
 vim.g.mapleader = " "
+-- Buffer local mappings or operations local to your current line?
+vim.g.maplocalleader = "\\"
 
 require("diagnostic")
 require("plugins")
