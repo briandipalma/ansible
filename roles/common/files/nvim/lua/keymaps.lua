e6bd150dc954d56cbc573ee2ee2f5fac39ba1192
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-12-14 20:02:15 +0000

    fix(nvim): Workaround neovim Ctrl-c bug

--- roles/common/files/nvim/lua/keymaps.lua
+++ roles/common/files/nvim/lua/keymaps.lua
@@ -22,3 +22,5 @@ vim.api.nvim_set_keymap("n", "<Left>", "<C-w>h", {})
 vim.api.nvim_set_keymap("n", "<Right>", "<C-w>l", {})
 vim.api.nvim_set_keymap("n", "<Up>", "<C-w>k", {})
 vim.api.nvim_set_keymap("n", "<Down>", "<C-w>j", {})
+-- https://github.com/neovim/neovim/issues/16416 workaround
+vim.api.nvim_set_keymap("n", "<C-c>", "<Esc>", {})
