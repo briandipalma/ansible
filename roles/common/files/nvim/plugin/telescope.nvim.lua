55c821c32c86450f641ba08a58a03b87d8592544
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2022-05-26 11:09:15 +0100

    feat(nvim): Add cycle previewer action to Telescope for git messages

--- roles/common/files/nvim/plugin/telescope.nvim.lua
+++ roles/common/files/nvim/plugin/telescope.nvim.lua
@@ -1,1 +1,14 @@
-require("telescope").load_extension("fzf")
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+
+telescope.setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-s>"] = actions.cycle_previewers_next,
+				["<C-a>"] = actions.cycle_previewers_prev,
+			},
+		},
+	},
+})
+telescope.load_extension("fzf")
