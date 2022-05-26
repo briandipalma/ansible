8b614025a3a623dc5819a06458efdf0c7ffa7398
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2022-05-26 10:42:11 +0100

    fix(nvim): Work around which-key closing Telescope prompt

--- roles/common/files/nvim/plugin/which-key.nvim.lua
+++ roles/common/files/nvim/plugin/which-key.nvim.lua
@@ -1,3 +1,17 @@
 local wk = require("which-key")
 
+local show = wk.show
+
+-- Work around for https://github.com/folke/which-key.nvim/issues/273
+wk.show = function(keys, opts)
+	if vim.bo.filetype == "TelescopePrompt" then
+		local map = "<c-r>"
+		local key = vim.api.nvim_replace_termcodes(map, true, false, true)
+
+		vim.api.nvim_feedkeys(key, "i", true)
+	end
+
+	show(keys, opts)
+end
+
 wk.setup({ spelling = { enabled = true } })
