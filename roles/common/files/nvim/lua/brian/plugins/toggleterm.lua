1e0e7d8fcf5cbaef1166bd58f36a4a02bdb7c26d
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2022-06-28 11:49:41 +0100

    refactor(nvim): Move which-key and toggleterm code into plugins

--- roles/common/files/nvim/lua/brian/plugins/toggleterm.lua
+++ roles/common/files/nvim/lua/brian/plugins/toggleterm.lua
@@ -1,1 +1,22 @@
-require("toggleterm").setup()
+local toggleterm = require("toggleterm")
+local Terminal = require("toggleterm.terminal").Terminal
+
+toggleterm.setup()
+
+local M = {}
+
+local lazygit = Terminal:new({
+	cmd = "lazygit",
+	direction = "float",
+	float_opts = {
+		-- Fill the entire screen
+		width = 400,
+		height = 200,
+	},
+})
+
+function M.lazygit_toggle()
+	lazygit:toggle()
+end
+
+return M
