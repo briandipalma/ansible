24b2408b47d00164f549d99d9c94105a5179d495
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2022-07-01 16:21:01 +0100

    feat(nvim): Add gitlint support

--- roles/common/files/nvim/lua/brian/plugins/null-ls.lua
+++ roles/common/files/nvim/lua/brian/plugins/null-ls.lua
@@ -1,15 +1,19 @@
 local null_ls = require("null-ls")
 
 local sources = {
+	null_ls.builtins.code_actions.proselint,
+	null_ls.builtins.diagnostics.fish,
+	null_ls.builtins.diagnostics.gitlint,
+	null_ls.builtins.diagnostics.markdownlint,
+	null_ls.builtins.diagnostics.proselint,
+	null_ls.builtins.diagnostics.selene,
+	null_ls.builtins.diagnostics.write_good,
+	null_ls.builtins.diagnostics.yamllint,
 	null_ls.builtins.formatting.fish_indent,
 	null_ls.builtins.formatting.prettierd,
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.formatting.trim_newlines,
 	null_ls.builtins.formatting.trim_whitespace,
-	null_ls.builtins.diagnostics.selene,
-	null_ls.builtins.diagnostics.write_good,
-	null_ls.builtins.diagnostics.markdownlint,
-	null_ls.builtins.diagnostics.yamllint,
 	null_ls.builtins.hover.dictionary,
 }
 
