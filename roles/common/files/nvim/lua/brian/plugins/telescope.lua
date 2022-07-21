b0cf75c1ebea7d9fee78a93383b77926a54d0122
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2022-07-21 22:53:01 +0100

    feat(nvim): Add a `cd` mapping to telescope

--- roles/common/files/nvim/lua/brian/plugins/telescope.lua
+++ roles/common/files/nvim/lua/brian/plugins/telescope.lua
@@ -11,6 +11,20 @@ telescope.setup({
 			},
 		},
 	},
+	pickers = {
+		find_files = {
+			mappings = {
+				n = {
+					["cd"] = function(prompt_bufnr)
+						local selection = require("telescope.actions.state").get_selected_entry()
+						local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+						require("telescope.actions").close(prompt_bufnr)
+						vim.cmd(string.format("silent cd %s", dir))
+					end,
+				},
+			},
+		},
+	},
 })
 
 telescope.load_extension("fzf")
