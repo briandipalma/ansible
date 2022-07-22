792954bf10351c618d269d61a02be6ab3e36de4f
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2022-07-22 13:39:59 +0100

    feat(nvim): Configure lualine

--- roles/common/files/nvim/lua/brian/plugins/lualine.lua
+++ roles/common/files/nvim/lua/brian/plugins/lualine.lua
@@ -2,4 +2,38 @@ local lualine = require("lualine")
 
 lualine.setup({
 	options = { theme = "rose-pine", globalstatus = true },
+	sections = {
+		lualine_a = {
+			{
+				"mode",
+				fmt = function(str)
+					return str:sub(1, 1)
+				end,
+			},
+		},
+		lualine_b = { "diff", "diagnostics" },
+		lualine_c = { { "filename", path = 1 } },
+		lualine_x = {
+			{
+				"encoding",
+				fmt = function(str)
+					if str == "utf-8" then
+						return
+					end
+
+					return str
+				end,
+			},
+			{
+				"fileformat",
+				fmt = function(str)
+					if str == "dos" then
+						return str
+					end
+				end,
+			},
+			"filetype",
+		},
+	},
+	extensions = { "nvim-tree", "quickfix", "toggleterm" },
 })
