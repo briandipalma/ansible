5b22b23e231e13f2e5c6567569037e90c29a724d
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-01-06 17:44:39 +0000

    refactor: Remove "cd" mapping from telescope as it's unused

--- roles/common/files/nvim/after/plugin/telescope.lua
+++ roles/common/files/nvim/after/plugin/telescope.lua
@@ -22,16 +22,6 @@ telescope.setup({
 	pickers = {
 		find_files = {
 			find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
-			mappings = {
-				n = {
-					["cd"] = function(prompt_bufnr)
-						local selection = require("telescope.actions.state").get_selected_entry()
-						local dir = vim.fn.fnamemodify(selection.path, ":p:h")
-						require("telescope.actions").close(prompt_bufnr)
-						vim.cmd(string.format("silent cd %s", dir))
-					end,
-				},
-			},
 		},
 		lsp_references = { fname_width = 70 },
 	},
