ec75e2b54816c06e963ce05e139b225abf151cca
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-01-12 18:23:36 +0000

    fix(nvim): Format Rust code on save

--- roles/common/files/nvim/after/plugin/null-ls.lua
+++ roles/common/files/nvim/after/plugin/null-ls.lua
@@ -25,6 +25,7 @@ local sources = {
 	null_ls.builtins.diagnostics.yamllint,
 	null_ls.builtins.formatting.fish_indent,
 	null_ls.builtins.formatting.prettierd,
+	null_ls.builtins.formatting.rustfmt,
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.hover.dictionary,
 }
