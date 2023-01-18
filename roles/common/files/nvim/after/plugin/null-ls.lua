640515bba192e59486defbbbf1c964137c20c0bc
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-01-18 14:31:24 +0000

    Feat(nvim): Remove unnecessary code linters

--- roles/common/files/nvim/after/plugin/null-ls.lua
+++ roles/common/files/nvim/after/plugin/null-ls.lua
@@ -19,19 +19,11 @@ local on_attach = function(client, bufnr)
 end
 
 local sources = {
-	null_ls.builtins.code_actions.proselint,
 	null_ls.builtins.diagnostics.fish,
-	null_ls.builtins.diagnostics.gitlint,
-	null_ls.builtins.diagnostics.markdownlint,
-	null_ls.builtins.diagnostics.proselint,
-	null_ls.builtins.diagnostics.selene,
-	null_ls.builtins.diagnostics.write_good,
-	null_ls.builtins.diagnostics.yamllint,
 	null_ls.builtins.formatting.fish_indent,
 	null_ls.builtins.formatting.prettierd,
 	null_ls.builtins.formatting.rustfmt,
 	null_ls.builtins.formatting.stylua,
-	null_ls.builtins.hover.dictionary,
 }
 
 null_ls.setup({ sources = sources, on_attach = on_attach })
