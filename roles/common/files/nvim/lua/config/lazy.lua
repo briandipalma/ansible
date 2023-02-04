2f9647a6c26af0416e6b189c458f0ad6e681288b
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-02-04 23:16:48 +0000

    feat(nvim): Ensure base LSPs and formatters are installed with Mason

--- roles/common/files/nvim/lua/config/lazy.lua
+++ roles/common/files/nvim/lua/config/lazy.lua
@@ -17,29 +17,11 @@ require("lazy").setup({
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
-	defaults = {
-		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-		lazy = false,
-		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-		-- have outdated releases, which may break your Neovim install.
-		version = false, -- always use the latest git commit
-		-- version = "*", -- try installing the latest stable version for plugins that support semver
-	},
 	checker = { enabled = true }, -- automatically check for plugin updates
 	performance = {
 		rtp = {
 			-- disable some rtp plugins
-			disabled_plugins = {
-				"gzip",
-				-- "matchit",
-				-- "matchparen",
-				-- "netrwPlugin",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
+			disabled_plugins = { "gzip", "netrwPlugin", "tarPlugin", "tohtml", "tutor", "zipPlugin" },
 		},
 	},
 })
---
+++ roles/common/files/nvim/lua/plugins/mason.lua
@@ -0,0 +1,15 @@
+return {
+	"williamboman/mason.nvim",
+	opts = {
+		ensure_installed = {
+			"ansible-language-server",
+			"ltex-ls",
+			"lua-language-server",
+			"marksman",
+			"prettierd",
+			"stylua",
+			"yaml-language-server",
+			"yamllint",
+		},
+	},
+}
