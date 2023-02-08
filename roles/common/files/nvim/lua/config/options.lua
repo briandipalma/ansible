f94b8563bdf39091c14e4da76d457139f4920ec8
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-02-08 21:32:31 +0000

    feat(nvim): Disable indent-blankline

--- roles/common/files/nvim/lua/config/options.lua
+++ roles/common/files/nvim/lua/config/options.lua
@@ -4,5 +4,6 @@
 local set = vim.opt
 
 set.colorcolumn = "80" -- Show column to help limit rightwards drift
+set.list = false -- Don't display whitespace characters
 set.scrolloff = 100 -- Number of lines buffer to keep above and below cursor line when scrolling
 set.winbar = "%=%m %f" -- Content of window bar, shown at the top of every window
---
+++ roles/common/files/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+	{ "folke/tokyonight.nvim", enabled = false },
+	{ "catppuccin/nvim", enabled = false },
+	{ "lukas-reineke/indent-blankline.nvim", enabled = false },
+}
