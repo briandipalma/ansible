0db693588488b3ffdfa6474b6cd6d64e741dce52
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-02-09 17:23:25 +0000

    feat(nvim): Disable remote plugin support

--- roles/common/files/nvim/lua/config/options.lua
+++ roles/common/files/nvim/lua/config/options.lua
@@ -19,3 +19,8 @@ set.winbar = "%=%m %f" -- Content of window bar, shown at the top of every windo
 vim.o.foldcolumn = "0" -- Hide the fold level numbers and fold markers...
 vim.o.foldlevel = 99 -- Only close ludicrously nested code automatically
 vim.o.foldlevelstart = 99
+
+vim.g.loaded_node_provider = 0 -- Disable Node.js remote plugins support
+vim.g.loaded_perl_provider = 0 -- Disable Perl remote plugins support
+vim.g.loaded_python3_provider = 0 -- Disable Python remote plugins support
+vim.g.loaded_ruby_provider = 0 -- Disable Ruby remote plugins support
