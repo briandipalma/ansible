c6d7f50c801f5322c7e425e73159698e0e7d2eae
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-02-16 22:06:09 +0000

    refactor(nvim): Improve dir-telescope keymapping descriptions

--- roles/common/files/nvim/lua/plugins/dir-telescope.lua
+++ roles/common/files/nvim/lua/plugins/dir-telescope.lua
@@ -3,7 +3,7 @@ return {
 	config = true,
 	dependencies = "nvim-telescope/telescope.nvim",
 	keys = {
-		{ "<leader>fD", "<cmd>FileInDirectory<cr>", desc = "List files in specified directory" },
-		{ "<leader>sD", "<cmd>GrepInDirectory<cr>", desc = "Searches in selected directory for string" },
+		{ "<leader>fD", "<cmd>FileInDirectory<cr>", desc = "Find Files (user selected dir)" },
+		{ "<leader>sD", "<cmd>GrepInDirectory<cr>", desc = "Grep (user selected dir)" },
 	},
 }
