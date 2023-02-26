d9b900ea03a42f2877be3e3722db5863bbee5d3a
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-02-26 22:58:42 +0000

    feat(nvim): Enable and configure text wrapping

--- roles/common/files/nvim/lua/config/options.lua
+++ roles/common/files/nvim/lua/config/options.lua
@@ -19,6 +19,11 @@ vim.o.foldcolumn = "0" -- Hide the fold level numbers and fold markers...
 vim.o.foldlevel = 99 -- Only close ludicrously nested code automatically
 vim.o.foldlevelstart = 99
 
+set.wrap = true -- Wrap text
+set.breakindentopt = "shift:2" -- Wrapped line's beginning will be shifted to emphasize line continuation
+set.linebreak = true -- Wrap long lines on whole words not the last character that fits on the screen
+set.showbreak = "↳" -- String to put at start of wrapped lines
+
 vim.g.loaded_node_provider = 0 -- Disable Node.js remote plugins support
 vim.g.loaded_perl_provider = 0 -- Disable Perl remote plugins support
 vim.g.loaded_python3_provider = 0 -- Disable Python remote plugins support
