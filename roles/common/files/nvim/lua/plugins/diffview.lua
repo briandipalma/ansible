6206aca5f92154e82e3e743660177b2db982ac5d
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-02-28 17:49:45 +0000

    feat(nvim): Add <leader>dr keymap to review current branch versus remote master

--- roles/common/files/nvim/lua/plugins/diffview.lua
+++ roles/common/files/nvim/lua/plugins/diffview.lua
@@ -1,11 +1,23 @@
+local width = math.max(35, vim.o.columns - 215)
+
 return {
 	"sindrets/diffview.nvim",
 	dependencies = "nvim-lua/plenary.nvim",
 	keys = {
-		{ "<leader>dh", "<cmd>DiffviewFileHistory<cr>", desc = "View current branch history" },
 		{ "<leader>df", "<cmd>DiffviewFileHistory %<cr>", desc = "View current file history" },
+		{ "<leader>dh", "<cmd>DiffviewFileHistory<cr>", desc = "View current branch history" },
+		{
+			"<leader>dr",
+			"<cmd>DiffviewOpen origin/master...HEAD<cr>",
+			desc = "Review current branch versus remote master",
+		},
 	},
 	opts = {
+		file_panel = {
+			win_config = {
+				width = width,
+			},
+		},
 		file_history_panel = {
 			log_options = {
 				git = {
