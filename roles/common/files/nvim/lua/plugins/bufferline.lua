a104176ea54cd913dc1a96d0940891efe88d10c4
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-03-01 22:23:09 +0000

    feat(nvim): Add arrow icon to selected buffer tab

---
+++ roles/common/files/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,13 @@
+return {
+	"akinsho/bufferline.nvim",
+	opts = {
+		highlights = {
+			indicator_selected = {
+				fg = "#0000FF",
+			},
+		},
+		options = {
+			indicator = { icon = "▶", style = "icon" },
+		},
+	},
+}
