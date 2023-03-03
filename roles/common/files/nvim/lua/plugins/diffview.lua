7192f263eaab366d9992331900047d32933efcaa
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-03-03 12:23:42 +0000

    feat(nvim): Add search file/branch commits for changes

--- roles/common/files/nvim/lua/plugins/diffview.lua
+++ roles/common/files/nvim/lua/plugins/diffview.lua
@@ -1,11 +1,34 @@
 local width = math.max(35, vim.o.columns - 215)
 
+local function searchFunction(path)
+	vim.ui.input({ prompt = "Search regex" }, function(query)
+		path = path or ""
+		query = query or ""
+
+		if query ~= "" then
+			query = string.format(" -G'%s'", query)
+		end
+
+		vim.cmd("DiffviewFileHistory " .. path .. query)
+	end)
+end
+
+local function searchFile()
+	searchFunction("%")
+end
+
+local function searchBranch()
+	searchFunction()
+end
+
 return {
 	"sindrets/diffview.nvim",
 	dependencies = "nvim-lua/plenary.nvim",
 	keys = {
 		{ "<leader>df", "<cmd>DiffviewFileHistory %<cr>", desc = "View current file history" },
 		{ "<leader>dh", "<cmd>DiffviewFileHistory<cr>", desc = "View current branch history" },
+		{ "<leader>dg", searchFile, desc = "Search current file history" },
+		{ "<leader>dG", searchBranch, desc = "Search current branch history" },
 		{
 			"<leader>dr",
 			"<cmd>DiffviewOpen origin/master...HEAD<cr>",
@@ -13,22 +36,7 @@ return {
 		},
 	},
 	opts = {
-		file_panel = {
-			win_config = {
-				width = width,
-			},
-		},
-		file_history_panel = {
-			log_options = {
-				git = {
-					single_file = {
-						diff_merges = "off",
-					},
-				},
-			},
-		},
-		default_args = {
-			DiffviewFileHistory = { "--follow" },
-		},
+		file_panel = { win_config = { width = width } },
+		default_args = { DiffviewFileHistory = { "--follow", "--no-merges" } },
 	},
 }
