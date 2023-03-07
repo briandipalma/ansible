119c51fe4df9b5568cce61f0702904f6ebe829ae
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-03-07 17:50:56 +0000

    feat(nvim): Make telescope prompt transparent

--- roles/common/files/nvim/lua/plugins/rose-pine.lua
+++ roles/common/files/nvim/lua/plugins/rose-pine.lua
@@ -1,9 +1,18 @@
 return {
-	{ "rose-pine/neovim", name = "rose-pine", opts = { variant = "dawn" } },
 	{
-		"LazyVim/LazyVim",
+		"rose-pine/neovim",
+		name = "rose-pine",
 		opts = {
-			colorscheme = "rose-pine",
+			variant = "dawn",
+			-- Make telescope.nvim transparent
+			highlight_groups = {
+				TelescopeBorder = { fg = "highlight_high", bg = "none" },
+				TelescopeNormal = { bg = "none" },
+				TelescopePromptNormal = { bg = "base" },
+				TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+				TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+			},
 		},
 	},
+	{ "LazyVim/LazyVim", opts = { colorscheme = "rose-pine" } },
 }
