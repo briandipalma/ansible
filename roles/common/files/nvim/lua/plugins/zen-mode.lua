106689fa4691721d9bcd41391027af9b6d0cf3ad
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-03-10 18:36:11 +0000

    feat(nvim): Remove zen-mode as neo-tree can be used to center buffer

--- roles/common/files/nvim/lua/plugins/zen-mode.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"folke/zen-mode.nvim",
-	cmd = "ZenMode",
-	opts = {
-		plugins = { kitty = { enabled = true, font = "+2" } },
-	},
-	keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-}
