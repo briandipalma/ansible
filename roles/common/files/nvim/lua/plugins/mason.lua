aa7870b2bd183a42bb18423d144b1a71946e2011
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-03-15 22:40:10 +0000

    feat(nvim): Don't install python treesitter/formatting tooling

--- roles/common/files/nvim/lua/plugins/mason.lua
+++ roles/common/files/nvim/lua/plugins/mason.lua
@@ -1,16 +1,26 @@
+local function indexOf(array, value)
+	for index, val in ipairs(array) do
+		if val == value then
+			return index
+		end
+	end
+end
+
 return {
 	"williamboman/mason.nvim",
-	opts = {
-		ensure_installed = {
-			"ansible-language-server",
-			"ansible-lint",
-			"css-lsp",
-			"ltex-ls",
-			"lua-language-server",
-			"marksman",
-			"stylua",
-			"terraform-ls",
-			"yamllint",
-		},
-	},
+	opts = function(_, opts)
+		local flake8Index = indexOf(opts.ensure_installed, "flake8")
+		local shfmtIndex = indexOf(opts.ensure_installed, "shfmt")
+
+		table.remove(opts.ensure_installed, flake8Index)
+		table.remove(opts.ensure_installed, shfmtIndex)
+		table.insert(opts.ensure_installed, "ansible-language-server")
+		table.insert(opts.ensure_installed, "ansible-lint")
+		table.insert(opts.ensure_installed, "css-lsp")
+		table.insert(opts.ensure_installed, "ltex-ls")
+		table.insert(opts.ensure_installed, "lua-language-server")
+		table.insert(opts.ensure_installed, "marksman")
+		table.insert(opts.ensure_installed, "terraform-ls")
+		table.insert(opts.ensure_installed, "yamllint")
+	end,
 }
