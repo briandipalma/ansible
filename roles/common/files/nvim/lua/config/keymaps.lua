79885b65d6be2525e53b9d4b8b05c0de6da4f5e2
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-04-24 17:33:34 +0100

    fix(nvim): Improve speed of handling `Esc` when using lazygit

--- roles/common/files/nvim/lua/config/keymaps.lua
+++ roles/common/files/nvim/lua/config/keymaps.lua
@@ -6,12 +6,8 @@ local fullScreenSize = { width = 1, height = 1 }
 
 -- lazygit, make it full screen, rest is the same as default
 vim.keymap.set("n", "<leader>gg", function()
-	Util.float_term({ "lazygit" }, { cwd = Util.get_root(), size = fullScreenSize })
+	Util.float_term({ "lazygit" }, { cwd = Util.get_root(), esc_esc = false, size = fullScreenSize })
 end, { desc = "Lazygit (root dir)" })
-
-vim.keymap.set("n", "<leader>gG", function()
-	Util.float_term({ "lazygit" }, { size = fullScreenSize })
-end, { desc = "Lazygit (cwd)" })
 
 vim.keymap.set("n", "<leader>fc", function()
 	vim.cmd.e(vim.fn.getreg("+"))
