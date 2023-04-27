9228d15d455f0e0789818cb18b08622d72511f6c
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-04-27 23:23:04 +0100

    feat(nvim): Make LSP references file path name longer, fullscreen telescope

--- roles/common/files/nvim/lua/plugins/dir-telescope.lua
+++ roles/common/files/nvim/lua/plugins/dir-telescope.lua
@@ -4,6 +4,6 @@ return {
 	dependencies = "nvim-telescope/telescope.nvim",
 	keys = {
 		{ "<leader>fD", "<cmd>FileInDirectory<cr>", desc = "Find Files (user selected dir)" },
-		{ "<leader>sD", "<cmd>GrepInDirectory<cr>", desc = "Grep (user selected dir)" },
+		{ "<leader>sf", "<cmd>GrepInDirectory<cr>", desc = "Grep (user selected folder)" },
 	},
 }
--- roles/common/files/nvim/lua/plugins/telescope.lua
+++ roles/common/files/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-telescope/telescope.nvim",
 	opts = {
 		defaults = {
-			layout_config = { height = 0.95, width = 0.95, vertical = { preview_height = 0.65 } },
+			layout_config = { height = 400, width = 400, vertical = { preview_height = 0.65 } },
 			layout_strategy = "vertical",
 			mappings = {
 				i = {
@@ -10,6 +10,9 @@ return {
 					["<a-p>"] = require("telescope.actions.layout").cycle_layout_prev,
 				},
 			},
+		},
+		pickers = {
+			lsp_references = { fname_width = 100 },
 		},
 	},
 }
