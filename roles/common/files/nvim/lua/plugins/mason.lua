8034cf90686c29c9a2284504ba371236fd109ad5
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-05-31 22:43:23 +0100

    feat(nvim): Add kotlin and Java LSPs

--- roles/common/files/nvim/lua/plugins/mason.lua
+++ roles/common/files/nvim/lua/plugins/mason.lua
@@ -17,6 +17,8 @@ return {
 		table.insert(opts.ensure_installed, "ansible-language-server")
 		table.insert(opts.ensure_installed, "ansible-lint")
 		table.insert(opts.ensure_installed, "css-lsp")
+		table.insert(opts.ensure_installed, "jdtls")
+		table.insert(opts.ensure_installed, "kotlin-language-server")
 		table.insert(opts.ensure_installed, "ltex-ls")
 		table.insert(opts.ensure_installed, "lua-language-server")
 		table.insert(opts.ensure_installed, "marksman")
