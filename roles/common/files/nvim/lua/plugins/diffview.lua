5f2d206d285e17ee6d8656974b169a7319709445
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-06-20 19:51:43 +0100

    feat!(nvim): Remove search diffview mappings as diffview has them builtin

--- roles/common/files/nvim/lua/plugins/diffview.lua
+++ roles/common/files/nvim/lua/plugins/diffview.lua
@@ -1,26 +1,5 @@
 local file_panel_win_config = { height = 28, position = "top" }
 
-local function searchFunction(path)
-	vim.ui.input({ prompt = "Search regex" }, function(query)
-		path = path or ""
-		query = query or ""
-
-		if query ~= "" then
-			query = string.format(" -G'%s'", query)
-		end
-
-		vim.cmd("DiffviewFileHistory " .. path .. query)
-	end)
-end
-
-local function searchFile()
-	searchFunction("%")
-end
-
-local function searchBranch()
-	searchFunction()
-end
-
 return {
 	"sindrets/diffview.nvim",
 	dependencies = "nvim-lua/plenary.nvim",
@@ -33,13 +12,7 @@ return {
 			mode = "v",
 			desc = "View current visual selection history",
 		},
-		{ "<leader>dg", searchFile, desc = "Search current file history" },
-		{ "<leader>dG", searchBranch, desc = "Search current branch history" },
-		{
-			"<leader>dd",
-			"<cmd>DiffviewOpen<cr>",
-			desc = "Review git working directory/index",
-		},
+		{ "<leader>dd", "<cmd>DiffviewOpen<cr>", desc = "Review git working directory/index" },
 		{
 			"<leader>dr",
 			"<cmd>DiffviewOpen origin/HEAD...HEAD<cr>",
