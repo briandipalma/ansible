5e0fd21f7b9a0132333d719502f8156c8b2ecd12
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-07-07 17:52:57 +0100

    feat(nvim): Add terraform extra

--- roles/common/files/nvim/lua/config/lazy.lua
+++ roles/common/files/nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.editor.mini-files" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
+		{ import = "lazyvim.plugins.extras.lang.terraform" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
 		{ import = "lazyvim.plugins.extras.ui.mini-animate" },
