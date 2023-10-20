cd6a093d7c43b1ea3d12b8b5edf3e8aeef808330
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-10-20 09:22:45 +0100

    feat(nvim): Enable bufferline

--- roles/common/files/nvim/lua/plugins/disabled.lua
+++ roles/common/files/nvim/lua/plugins/disabled.lua
@@ -2,6 +2,5 @@ return {
 	{ "folke/tokyonight.nvim", enabled = false },
 	{ "catppuccin/nvim", enabled = false },
 	{ "lukas-reineke/indent-blankline.nvim", enabled = false },
-	{ "akinsho/bufferline.nvim", enabled = false },
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
