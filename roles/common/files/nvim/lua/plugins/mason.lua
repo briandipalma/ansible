ccf959f010f8a7e232206f2e61b4f7911daf74a1
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-10-20 09:27:28 +0100

    chore(nvim): Remove marksman from mason config - md extra installs it

--- roles/common/files/nvim/lua/plugins/mason.lua
+++ roles/common/files/nvim/lua/plugins/mason.lua
@@ -7,6 +7,5 @@ return {
 		table.insert(opts.ensure_installed, "kotlin-language-server")
 		table.insert(opts.ensure_installed, "ltex-ls")
 		table.insert(opts.ensure_installed, "lua-language-server")
-		table.insert(opts.ensure_installed, "marksman")
 	end,
 }
