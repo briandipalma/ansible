c2a8f605c5839ff7e388866222897a038c3fd9cf
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-11-06 17:51:43 +0000

    feat(nvim): Use neogit for git operations by default
    Update lock file

--- roles/common/files/nvim/lua/plugins/neogit.lua
+++ roles/common/files/nvim/lua/plugins/neogit.lua
@@ -4,16 +4,25 @@ return {
 	cmd = "Neogit",
 	config = true,
 	opts = {
+		-- Enter insert mode if no commit message
+		disable_insert_on_commit = "auto",
 		use_per_project_settings = false,
+		-- Persist force with lease
 		ignored_settings = {},
 		popup = {
 			kind = "split_above",
 		},
-		integrations = {
-			diffview = true,
-		},
 	},
 	keys = {
+		{
+			"<leader>gg",
+			function()
+				local neogit = require("neogit")
+
+				neogit.open()
+			end,
+			desc = "Neogit",
+		},
 		{
 			"<leader>gc",
 			function()
