ad4a197850e4be58873b16af1ff61ef34ccb6693
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-11-16 22:47:17 +0000

    feat(nvim): Use lualine for winbar

--- roles/common/files/nvim/lua/config/options.lua
+++ roles/common/files/nvim/lua/config/options.lua
@@ -11,8 +11,6 @@ set.breakindentopt = "shift:2" -- Wrapped line's beginning will be shifted to em
 set.linebreak = true -- Wrap long lines on whole words not the last character that fits on the screen
 set.showbreak = "↳" -- String to put at start of wrapped lines
 
-set.winbar = "%=%m %f"
-
 vim.g.loaded_node_provider = 0 -- Disable Node.js remote plugins support
 vim.g.loaded_perl_provider = 0 -- Disable Perl remote plugins support
 vim.g.loaded_python3_provider = 0 -- Disable Python remote plugins support
---
+++ roles/common/files/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,20 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	opts = function(_, opts)
+		local winbar_conf = {
+			lualine_y = {
+				{
+					"filetype",
+					icon_only = true,
+					separator = { left = "" },
+				},
+				{ "filename", path = 1, color = { fg = "Statement", gui = "bold" } },
+			},
+		}
+
+		opts.options.disabled_filetypes.winbar =
+			{ "dashboard", "NeogitStatus", "DiffviewFiles", "Trouble", "NeogitCommitMessage" }
+		opts.winbar = winbar_conf
+		opts.inactive_winbar = winbar_conf
+	end,
+}
