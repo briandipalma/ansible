682dc0105fd1bc1fa717d0d387a6d9e9d557f416
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-01-09 17:18:08 +0000

    feat(neovim): Disable catppuccin

--- roles/common/files/nvim/lua/plugins/disabled.lua
+++ roles/common/files/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
 	{ "folke/tokyonight.nvim", enabled = false },
-	{ "catppuccin/nvim", enabled = false },
+	{ "catppuccin", enabled = false },
 	{ "lukas-reineke/indent-blankline.nvim", enabled = false },
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
