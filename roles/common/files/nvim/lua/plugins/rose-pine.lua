f507f1212e498a4bc8215b2c7944b0c2bf272082
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-01-16 12:41:54 +0000

    feat(nvim): Dim inactive windows in rose-pine

--- roles/common/files/nvim/lua/plugins/rose-pine.lua
+++ roles/common/files/nvim/lua/plugins/rose-pine.lua
@@ -4,13 +4,17 @@ return {
 		name = "rose-pine",
 		opts = {
 			variant = "dawn",
+			dim_inactive_windows = true,
+			styles = {
+				transparency = true,
+			},
 			-- Make telescope.nvim transparent
 			highlight_groups = {
-				BufferLineTabSelected = { fg = "text" },
 				TelescopeBorder = { fg = "highlight_high", bg = "none" },
 				TelescopeNormal = { bg = "none" },
 				TelescopePromptNormal = { bg = "base" },
 				TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+				TelescopeSelection = { fg = "text", bg = "base" },
 				TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
 			},
 		},
